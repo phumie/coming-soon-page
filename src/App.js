d32483ba9ef35a40fca6a633c251a071e2e969ad
Phumie Nevhutala <phumie.nevhutala@gmail.com>
2019-02-19 09:23:45 +0200

    Coming soon web page complete.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import { Grid, Cell } from "react-mdl";
+import windowSize from "react-window-size";
+import logo from "./assets/phumie-logo-white.png";
+import "./App.css";
+
+const particleOpt = {
+  particles: {
+    number: {
+      value: 30
+    },
+    size: {
+      value: 3
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+        <div
+          className="header-content"
+          style={{ width: "100%", margin: "auto" }}
+        >
+          <Particles
+            params={particleOpt}
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%"
+            }}
+          />
+          <div className="contents">
+            <img src={logo} />
+            <div className="coming-soon">
+              <h3>WEB DEVELOPMENT | SOFTWARE DEVELOPMENT | UI/UX DESINGING</h3>
+              <p>WEBSITE UNDER CONSTRUCTION</p>
+              <hr />
+            </div>
+          </div>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            href="#aboutme"
+            id="scroll-next"
+            className="scroll-down"
+            address="true"
+          />
+        </div>
       </div>
     );
   }
